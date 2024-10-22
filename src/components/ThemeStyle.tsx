1a93c2b4eacc4d4b45ae1589da207eebe43a8c84
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-10-22 23:33:20 +0300

    Create ThemeStyle. Delete style.css

---
+++ src/components/ThemeStyle.tsx
@@ -0,0 +1,24 @@
+const theme = {
+  color: {
+    primary: "hsl(14, 86%, 42%)",
+    secondary: "hsl(159, 69%, 38%)",
+    natural_50: "hsl(20, 50%, 98%)",
+    natural_100: "hsl(13, 31%, 94%)",
+    natural_300: "hsl(14, 25%, 72%)",
+    natural_400: "hsl(7, 20%, 60%)",
+    natural_500: "hsl(12, 20%, 44%)",
+    natural_900: "hsl(14, 65%, 9%)",
+  },
+
+  font: {
+    size: "16px",
+    family: "Red Hat Text, sans-serif",
+    weight: {
+      normal: 400,
+      semi_bold: 600,
+      bold: 700,
+    },
+  },
+};
+
+export { theme };
