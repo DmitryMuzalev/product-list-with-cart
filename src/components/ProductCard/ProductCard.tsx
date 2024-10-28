7debab9889020ea2c0fb016b1e9c8741eaf58ad6
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-10-28 23:14:26 +0300

    Delete test data

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,24 +3,24 @@ import { ProductCardImage } from "./ProductCardImage";
 import { ProductCardInfo } from "./ProductCardInfo";
 import { useState } from "react";
 
-const testProduct = {
-  image: {
-    thumbnail: "./images/image-creme-brulee-thumbnail.jpg",
-    mobile: "./images/image-creme-brulee-mobile.jpg",
-    tablet: "./images/image-creme-brulee-tablet.jpg",
-    desktop: "./images/image-creme-brulee-desktop.jpg",
-  },
-  name: "Vanilla Bean Crème Brûlée",
-  category: "Crème Brûlée",
-  price: 7.0,
-};
-
 const Wrapper = styled.article`
-  width: 250px;
+  width: 100%;
   overflow: hidden;
 `;
 
-function ProductCard() {
+type Product = {
+  image: {
+    thumbnail: string;
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+  category: string;
+  price: number;
+};
+
+function ProductCard({ productData }: Product) {
   const [counter, setCounter] = useState(0);
 
   const increment = () => setCounter((prev) => prev + 1);
@@ -28,9 +28,9 @@ function ProductCard() {
 
   return (
     <Wrapper>
-      <ProductCardImage {...testProduct} counter={counter} />
+      <ProductCardImage {...productData} counter={counter} />
       <ProductCardInfo
-        {...testProduct}
+        {...productData}
         counter={counter}
         increment={increment}
         decrement={decrement}
