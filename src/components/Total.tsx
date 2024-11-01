a12d1a8f7224120f37e3a3e21c18ad691d1a6211
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-11-01 23:01:26 +0300

    Create Total

---
+++ src/components/Total.tsx
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { selectCart } from "../feature/Cart/cart-slice";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 0;
+  column-gap: 1rem;
+
+  span {
+    font-weight: ${(props) => props.theme.font.weight.bold};
+    font-size: 1.5rem;
+  }
+`;
+
+function Total() {
+  const cartList = useSelector(selectCart);
+
+  const total = cartList.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  return (
+    <Wrapper>
+      <p>Order Total</p>
+      <span>${total.toFixed(2)}</span>
+    </Wrapper>
+  );
+}
+
+export { Total };
